65444a92f95c11d5d7e7efb4423c1c8c65d5a032
Vinay Kollikani <vinaykolkani7@gmail.com>
2024-01-30 21:59:51 +0530

    Completed Basics > Operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -44,3 +44,59 @@
 // * boolean => false => false => Boolean
 // * null => false => Boolean
 // * undefined => false => Boolean
+
+// ! ---------------- OPERATIONS ----------------
+
+let value1 = 10;
+let value2 = 2;
+// console.log("Addition : " + value1 + value2); //! 102
+// console.log("Addition : " + (value1 + value2)); //! 12
+// console.log("Subtraction : " + (value1 - value2)); //! 8
+// console.log("Multiplication : " + value1 * value2); //! 20
+// console.log("Division : " + value1 / value2); //! 5
+// console.log("Modulo/Remainder : " + (value1 % value2)); //! 0
+// console.log("Power : " + value1 ** value2); //! 100
+
+// console.log(7 + 9); // 16
+// console.log(7 + "9"); // 79
+// console.log("7" + 9); // 79
+// console.log("7" + "9"); // 79
+// console.log("2" + 6 + 4); // 264
+// console.log(4 + 6 + "4"); // 104
+// ? In the above cases, JavaScript Converts the values
+// into string or number based on their position.
+// ? If the first value is String, then the rest values
+// are also converted into Strings.
+// ? If the starting values are numbers, then operation
+// is done on the following numbers until a string value is found.
+
+// let str1 = "Hello ";
+// let str2 = "World";
+// let str3 = str1 + str2;
+// console.log(str3); // Hello World
+// let str1 = 107;
+// let str2 = "701";
+// let str3 = str1 + str2;
+// console.log(str3); // 107701
+
+// ! The following type of codes should be avoided, for readability
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log(9 + 7 * 45 / 5); // 72
+// let num1, num2, num3;
+// num1 = num2 = num3 =  7 + 5;
+// console.log(num1);
+
+// ? PostFix and Prefix Increment / Decrement
+
+// let num1 = 7;
+// console.log("Prefix Increment for num1 - 7 : " + ++num1); // 8 Increases in the same occurance
+// let num2 = 7;
+// console.log("Postfix Increment for num2 - 7 : " + num2++); // 7 Increases in the next occurance
+// console.log("Postfix Increment for num2 - next Occurance : " + num2); //8
+
+// let num1 = 7;
+// console.log("Prefix decrement for num1 - 7 : " + --num1); // 6 decreases in the same occurance
+// let num2 = 7;
+// console.log("Postfix decrement for num2 - 7 : " + num2--); // 7 decreases in the next occurance
+// console.log("Postfix decrement for num2 - next Occurance : " + num2); // 6

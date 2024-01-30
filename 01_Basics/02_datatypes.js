96c0675a28114a60578dcaf641b004d6a589843a
Vinay Kollikani <vinaykolkani7@gmail.com>
2024-01-30 20:44:18 +0530

    Completed Basics > DataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+// ! "use strict"; // Treats all JS code as newer version
+
+// alert("Hello World"); // This is valid only in Browser's JavaScript V8 engine not in Node
+// prompt("This is also not valid in Node")
+
+// ? Different Types of DataTypes in JavaScript
+/*
+ * Number => 9,8,78,4658
+ * boolean => true / false
+ * String => "Hello World"
+ * bigInt
+ * null => Standalone value (no value)
+ * undefined => value not defined
+ * symbol => used for unique
+ * objects (Non Primitive)
+ ? console.log(typeof variableName)
+ ? Type of Method/Function is used to find the datatype of the variable
+*/
+
+// let numEx = 718;
+// let stringEx = "Dhoni";
+// let booleanEx = true;
+// let nullEx = null;
+// let undefinedEx;
+// console.log(typeof numEx); // number
+// console.log(typeof stringEx); // string
+// console.log(typeof booleanEx); // boolean
+// console.log(typeof nullEx); // object
+// console.log(typeof undefinedEx); // undefined

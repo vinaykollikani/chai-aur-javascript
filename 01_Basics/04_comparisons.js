7fc28235c212928069d4134b7087d7bf6e41c49b
Vinay Kollikani <vinaykolkani7@gmail.com>
2024-02-01 22:21:06 +0530

    Completed Basics > Comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,30 @@
+// ! Comparison Operators in JavaScript
+// console.log(2 > 4); // false
+// console.log(2 < 4); // true
+// console.log("2" > 1); //  true
+// console.log("02" > 1); //  true
+// console.log("02" > "1"); // false
+// console.log("02" > "01"); // true
+// console.log(2 > 1); //  true
+// console.log(2 > "1"); //  true
+// console.log(2 > "01"); //  true
+// console.log(null > 0); // false
+// console.log(null < 0); // false
+// console.log(null >= 0); // true
+// console.log(null <= 0); // true
+// console.log(null == 0); // false
+// console.log(null != 0); // true
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined >= 0); // false
+// console.log(undefined <= 0); // false
+// console.log(undefined == 0); // false
+// console.log(undefined != 0); // true
+
+// * '==' : checks only the value of the variable
+// * '===' : checks the value of the variable and also the datatype of the variable i.e. Strict Checking
+// Example ::
+// console.log("1" == 1); // true
+// console.log("1" == "1"); // true
+// console.log("1" === 1); // false
+// console.log("1" === "1"); // true
